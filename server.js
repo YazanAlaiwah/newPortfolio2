82e0f358dd5a6376914906d27bc9c4fb7f9c2a5e
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-04-02 15:02:36 +0300

    test

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('./public'));
+
+// app.get('/',(req,res)=>{
+
+// })
+
+
+
+
+app.listen(PORT , ()=> console.log(`listening on port ${PORT}`))
